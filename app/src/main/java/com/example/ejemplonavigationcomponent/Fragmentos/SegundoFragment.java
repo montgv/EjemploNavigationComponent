5ac0ff0adf16299bb04efed85834bd924a845a45
estudiante <montserrat.garrido.220@estudiante.iesoretania.es>
2022-10-17 10:16:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ejemplonavigationcomponent/Fragmentos/SegundoFragment.java
@@ -0,0 +1,48 @@
+package com.example.ejemplonavigationcomponent.Fragmentos;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.ejemplonavigationcomponent.R;
+
+public class SegundoFragment extends Fragment {
+
+    public SegundoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_segundo, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final NavController navController = Navigation.findNavController(view);
+
+        final TextView txt1 = view.findViewById(R.id.textViewText);
+        final TextView txt2 = view.findViewById(R.id.textViewNumero);
+        final Button boton = view.findViewById(R.id.idBotonFinal);
+
+        txt1.setText(getArguments().getString("texto"));
+        txt2.setText(String.valueOf(getArguments().getInt("numero")));
+
+        
+    }
+}
---
+++ app/src/main/java/com/example/ejemplonavigationcomponent/Fragmentos/finalFragment.java
@@ -0,0 +1,25 @@
+package com.example.ejemplonavigationcomponent.Fragmentos;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ejemplonavigationcomponent.R;
+
+public class finalFragment extends Fragment {
+
+    public finalFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_final, container, false);
+    }
+}
